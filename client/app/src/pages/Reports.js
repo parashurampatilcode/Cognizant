12754781446f43c26a314ba155564b76fdc78683
Parashuram <parashurampatilgithub@gmail.com>
2025-04-04 07:47:06 +0000

    reports - removed function

--- client/app/src/pages/Reports.js
+++ client/app/src/pages/Reports.js
@@ -1,14 +1,9 @@
 // Reports.js
-// Commiting from Branch
 
 import React from "react";
 import { Typography } from "@mui/material";
 
 function Reports() {
-  return <Typography variant="h4">Reports - Work in Progress</Typography>;
-}
-
-function Reports1() {
   return <Typography variant="h4">Reports - Work in Progress</Typography>;
 }
 
