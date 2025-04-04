7dd0d494029870002c5c92b698e1c25fc2034f40
Manoodev <manoodev@yahoo.co.in>
2025-04-04 13:41:47 +0000

    First push

--- client/app/src/pages/DemandView.js
+++ client/app/src/pages/DemandView.js
@@ -3,7 +3,7 @@ import React from "react";
 import { Typography } from "@mui/material";
 
 function DemandView() {
-  return <Typography variant="h4">Demand View - Detailed View</Typography>;
+  return <Typography variant="h4">Demand View - Detailed View-</Typography>;
 }
 
 export default DemandView;
